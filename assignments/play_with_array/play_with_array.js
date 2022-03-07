22026b85ccdd4977b5f04ed0f8c2de7c2ad69853
Pavankumar624 <s.pavankumar022@gmail.com>
2022-03-07 13:20:29 +0530

    assignment5submission

--- assignments/play_with_array/play_with_array.js
+++ assignments/play_with_array/play_with_array.js
@@ -7,7 +7,13 @@ function getEven(arr) {
 
         Write you code below
     */
-
+   var k=[];
+   for(let i=0;i<arr.length;i++){
+       if(arr[i]%2===0){
+           k.push(arr[i])
+       }
+   }
+   return k;
 }
 
 function multiplyByN(arr, n) {
@@ -18,6 +24,8 @@ function multiplyByN(arr, n) {
             Output: [3,9,13,165]
         Write you code below
     */
+    mul=arr.map((k)=>k*n);
+    return mul
 }
 
 function removeNthElement(arr, n) {
@@ -28,6 +36,9 @@ function removeNthElement(arr, n) {
             Output: [1,3,4,7]
         Write you code below
     */
+   arr.splice(n,1);
+   return arr
+   
 }
 
 module.exports = {
